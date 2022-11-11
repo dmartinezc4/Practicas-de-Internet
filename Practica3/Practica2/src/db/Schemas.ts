df7d17289150b071b3a7265b1ca21f17df1d13b2
David <43136464+dmartinezc4@users.noreply.github.com>
2022-11-11 19:49:11 +0100

    Delete Practica3/Practica2 directory

--- Practica3/Practica2/src/db/Schemas.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectId } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-import {User,Book,Author} from "../types.ts";
-
-export type UserSchema = User & {_id: ObjectId};
-export type BookSchema = Book & {_id: ObjectId};
-export type AuthorSchema = Author & {_id: ObjectId};
